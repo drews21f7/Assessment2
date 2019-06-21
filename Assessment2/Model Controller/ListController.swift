bb1d7c86217895a605af6f98578c6c885ab4747b
Drew Seeholzer <drews21f7@gmail.com>
2019-06-21 10:33:54 -0600

    Finished view layout, attemted to add display alert functionality.

--- Assessment2/Model Controller/ListController.swift
+++ Assessment2/Model Controller/ListController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Drew Seeholzer. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import CoreData
 
 class ListController {
@@ -39,6 +39,21 @@ class ListController {
             try moc.save()
         } catch {
             print("Error saving to moc: \(error.localizedDescription)")
+        }
+    }
+}
+
+extension ListController {
+    func displayAlert() {
+        let alertController = UIAlertController(title: "Add Item", message: "Please add an item to your shopping list", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Enter your item..."
+        }
+        
+        let itemAdd = UIAlertAction(title: "Add", style: .default) { (_) in
+            guard let itemText = alertController.textFields?.text,
+                let item = addItem(item: UITextField) else {return}
         }
     }
 }
